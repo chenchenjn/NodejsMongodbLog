2dea06bc396dc4cb20590deed08e96f7492bd1bf
cjn <289617908@qq.com>
2021-12-15 16:26:49 -0800

    cjntest

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app=express()
 const mongoose = require('mongoose');//mongoose
-
+const ejs=require('mongoose')
 mongoose.connect('mongodb://172.21.2.236:27017/190110910503');//连接数据库
 
 const schema={
@@ -20,6 +20,12 @@ app.get("/input",(req,res)=>{
     console.log(req.query)
     const kitty = new mydata({ name:req.query.first,health:req.query.second });//他写进去了吗？
     kitty.save()
+    // ejs.renderFile(filename, data, options, function(err, str){
+    //     // str => 输出渲染后的 HTML 字符串
+    // });
+    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+    res.send(str)
+    })
 })
 // app.get("/",(req,res)=>{
 //     res.send("hello")
